c50ccf898b6c8bb44a6591848253169b7ee2694e
PoojaDL <poojadl2002@gmail.com>
2023-08-30 17:45:38 +0530

    Adding increment and decrement by 2 in redux Action

--- redux-demo.js
+++ redux-demo.js
@@ -1,11 +1,11 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 5 };
+  if (action.type === "incrementBy2") {
+    return { counter: state.counter + 2 };
   }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+  if (action.type === "decrementBy2") {
+    return { counter: state.counter - 2 };
   }
 };
 
@@ -17,5 +17,5 @@ const subscriber = () => {
 };
 
 store.subscribe(subscriber);
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
+store.dispatch({ type: "incrementBy2" });
+store.dispatch({ type: "decrementBy2" });
